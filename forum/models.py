bff68d228bba24bf293b75f307cf5da2fd1dafc1
sumanpatra970 <patrasuman992@gmail.com>
2022-03-01 09:59:29 +0530

    Update models.py

--- forum/models.py
+++ forum/models.py
@@ -31,6 +31,7 @@ class Donation(models.Model):
     Name = models.CharField(max_length=90,default="")
     Email = models.CharField(max_length=50,blank=True,default="")
     Amount = models.IntegerField(default=100)
+    Date = models.DateTimeField(auto_now_add=True)
     Mobileno = models.CharField(max_length=100,default="",blank=True)
     made_on = models.DateTimeField(auto_now_add=True)
 
