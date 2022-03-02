e8081399bf300e39b3b93643e92e0224568531e3
sumanpatra970 <patrasuman992@gmail.com>
2022-03-02 15:31:49 +0530

    Update views.py

--- community/views.py
+++ community/views.py
@@ -3,3 +3,351 @@ from django.shortcuts import render
 # Create your views here.
 def shree(request):
     return render(request,'shree.html')
+
+def rahul(request):
+    return render(request,'rahul.html')
+
+def radhakant(request):
+    return render(request,'radhakant.html')
+
+def abhijit(request):
+    return render(request,'abhijit.html')
+
+def kavitha(request):
+    return render(request,'kavitha.html')
+
+def nivi(request):
+    return render(request,'nivi.html')
+
+def shashank(request):
+    return render(request,'shashank.html')
+
+def jyoti(request):
+    return render(request,'jyoti.html')
+
+def sumant(request):
+    return render(request,'sumant.html')
+
+def juned(request):
+    return render(request,'juned.html')
+
+def soni(request):
+    return render(request,'soni.html')
+
+def koushik(request):
+    return render(request,'koushik.html')
+
+def pragati(request):
+    return render(request,'pragati.html')
+
+def karan(request):
+    return render(request,'karan.html')
+
+def bikram(request):
+    return render(request,'bikram.html')
+
+def goutam(request):
+    return render(request,'goutam.html')
+
+def shuv(request):
+    return render(request,'shuv.html')
+
+def shradha(request):
+    return render(request,'shradha.html')
+
+def popin(request):
+    return render(request,'popin.html')
+
+def loops(request):
+    return render(request,'loops.html')
+
+def damini(request):
+    return render(request,'damini.html')
+
+def sujata(request):
+    return render(request,'sujata.html')
+
+def aayusi(request):
+    return render(request,'aayushi.html')
+
+def sukesh(request):
+    return render(request,'sukesh.html')
+
+def piyush(request):
+    return render(request,'prityush.html')
+
+def priyanka(request):
+    return render(request,'priyanka.html')
+
+def ahana(request):
+    return render(request,'ahana.html')
+
+def rispa(request):
+    return render(request,'rispa.html')
+
+def jawat(request):
+    return render(request,'jawat.html')
+
+def manan(request):
+    return render(request,'manan.html')
+
+def swapansu(request):
+    return render(request,'sapansu.html')
+
+def srimaa(request):
+    return render(request,'srimaa.html')
+
+def abhya(request):
+    return render(request,'abhya.html')
+
+def anupam(request):
+    return render(request,'anupam.html')
+
+def anindya(request):
+    return render(request,'anindya.html')
+
+def krutee(request):
+    return render(request,'krutee.html')
+
+def sandhya(request):
+    return render(request,'sandhya.html')
+
+def tejaswi(request):
+    return render(request,'tejaswi.html')
+
+def farheen(request):
+    return render(request,'farheen.html')
+
+def darshini(request):
+    return render(request,'darshini.html')
+
+def jita(request):
+    return render(request,'jita.html')
+
+def smita(request):
+    return render(request,'smita.html')
+
+def sanjana(request):
+    return render(request,'sanjana.html')
+
+def padmini(request):
+    return render(request,'padmini.html')
+
+def saloni(request):
+    return render(request,'saloni.html')
+
+def soumya(request):
+    return render(request,'soumya.html')
+
+def abhinash(request):
+    return render(request,'abhinash.html')
+
+def hardeek(request):
+    return render(request,'hardeek.html')
+
+def deekshith(request):
+    return render(request,'deekshith.html')
+
+def bharatendu(request):
+    return render(request,'bharatendu.html')
+
+def animesh(request):
+    return render(request,'animesh.html')
+
+def abhipsa(request):
+    return render(request,'abhipsa.html')
+
+def ajay(request):
+    return render(request,'ajay.html')
+
+def amit_patjoshi(request):
+    return render(request,'amit_patjoshi.html')
+
+def richa(request):
+    return render(request,'richa.html')
+
+def priynka_br(request):
+    return render(request,'priynka_br.html')
+
+def soumya_prusti(request):
+    return render(request,'soumya_prusti.html')
+
+def tikeshwar(request):
+    return render(request,'tikeshwar.html')
+
+def pallavi(request):
+    return render(request,'pallavi.html')
+
+def sidhi(request):
+    return render(request,'sidhi.html')
+
+def soumav(request):
+    return render(request,'soumav.html')
+
+def soumyashree(request):
+    return render(request,'soumyashree.html')
+
+def asd(request):
+    return render(request,'asd.html')
+
+def prachi(request):
+    return render(request,'prachi.html')
+
+def sushree(request):
+    return render(request,'sushree.html')
+
+def pallavii(request):
+    return render(request,'pallavii.html')
+
+def ayushi(request):
+    return render(request,'ayushi.html')
+
+def ayush(request):
+    return render(request,'ayush.html')
+
+def anukta(request):
+    return render(request,'anukta.html')
+
+def ssn(request):
+    return render(request,'ssn.html')
+
+def nirali(request):
+    return render(request,'nirali.html')
+
+def deeja(request):
+    return render(request,'deeja.html')
+
+def deepa(request):
+    return render(request,'deepa.html')
+
+def pp(request):
+    return render(request,'pp.html')
+
+def chetna(request):
+    return render(request,'chetna.html')
+
+def adityoli(request):
+    return render(request,'adityaoli.html')
+
+def prasanna(request):
+    return render(request,'prasanna.html')
+
+def albin(request):
+    return render(request,'albin.html')
+
+def subashis(request):
+    return render(request,'subashis.html')
+
+def nikhil(request):
+    return render(request,'nikhil.html')
+
+def asmita(request):
+    return render(request,'asmita.html')
+
+def trilochan(request):
+    return render(request,'trilochan.html')
+
+def jayesh(request):
+    return render(request,'jayesh.html')
+
+def saswat(request):
+    return render(request,'saswat.html')
+
+def rishi(request):
+    return render(request,'rishi.html')
+
+def manisha(request):
+    return render(request,'manisha.html')
+
+def rudra(request):
+    return render(request,'rudraa.html')
+
+def somensoni(request):
+    return render(request,'notfound.html')
+
+def tannya(request):
+    return render(request,'tannya.html')
+
+def pooja(request):
+    return render(request,'pooja.html')
+
+def haritha(request):
+    return render(request,'haritha.html')
+
+def gangaprasad(request):
+    return render(request,'ganga.html')
+
+def amal(request):
+    return render(request,'amal.html')
+
+def oshi(request):
+    return render(request,'oshi.html')
+
+def lipsaa(request):
+    return render(request,'lipsaa.html')
+
+def anand(request):
+    return render(request,'anand.html')
+
+def werika(request):
+    return render(request,'werika.html')
+
+def smaroki(request):
+    return render(request,'smaroki.html')
+
+def sanju(request):
+    return render(request,'sanju.html')
+
+def bvaditya(request):
+    return render(request,'bvaditya.html')
+
+def srivalli(request):
+    return render(request,'svp.html')
+
+def ritu(request):
+    return render(request,'ritu.html')
+
+def chiranjeeb(request):
+    return render(request,'chiranjeeb.html')
+
+def boaz(request):
+    return render(request,'boaz.html')
+
+def rahul2(request):
+    return render(request,'rahull.html')
+
+def apruba(request):
+    return render(request,'apruba.html')
+
+def nimai(request):
+    return render(request,'nimai.html')
+
+def sarthak(request):
+    return render(request,'sarthak.html')
+
+def suniti(request):
+    return render(request,'suniti.html')
+
+def sushil(request):
+    return render(request,'sushil.html')
+
+def sushant(request):
+    return render(request,'sushant.html')
+
+def raju(request):
+    return render(request,'raju.html')
+
+def shruti(request):
+    return render(request,'shruti.html')
+
+def sambit(request):
+    return render(request,'sambit.html')
+
+def rashmi(request):
+    return render(request,'rashmi.html')
+
+def utkal(request):
+    return render(request,'utkal.html')
+
+def akanshya(request):
+    return render(request,'akanshya.html')
